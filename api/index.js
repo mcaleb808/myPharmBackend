14b7fc2eb1ddbbab1c97c70a69e2b06c26b85cd0
Mugisha Caleb <mcaleb808@gmail.com>
2019-12-16 21:19:08 +0200

    fixes tests

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 import config from 'dotenv';
 import express from 'express';
-import PharmRoutes from './v1/routes/pharmRoutes';
+import pharmRoutes from './v1/routes/pharmRoutes';
 import requestsRoutes from './v1/routes/requestsRoutes';
 
 config.config();
@@ -10,7 +10,7 @@ const { PORT = 8000 } = process.env;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/api/v1/pharmacies', PharmRoutes);
+app.use('/api/v1/pharmacies', pharmRoutes);
 app.use('/api/v1/requests', requestsRoutes);
 
 app.get('/', (_req, res) => {
